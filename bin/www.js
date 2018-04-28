cca63cb1ee01cf07a41b0cfe350fa193da5262ea
alexsfig <alexandersosa2204@gmail.com>
2018-04-28 10:24:39 -0600

    Add file with conf_var

--- bin/www.js
+++ bin/www.js
@@ -1,13 +1,12 @@
 const http = require('http');
 const app = require('../app'); // The express app we just created
+const conf_var = require('../server/config/config_var.js'); // The express app we just created
 
 const port = parseInt(process.env.PORT, 10) || 8000;
-const jwt_secret = '771uidswi7dseeyfr6e7r6u';
-
 app.set('port', port);
-app.set('jwt_secret', jwt_secret);
+app.set('jwt_secret', conf_var.jwt_secret);
 
 const server = http.createServer(app);
 server.listen(port);
-
+console.log(conf_var.jwt_secret)
 console.log('Magic happens on port ' + port);
