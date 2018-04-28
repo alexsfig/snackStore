d7e4d2c859b63306872a09019cf72aafaef66f4c
alexsfig <alexandersosa2204@gmail.com>
2018-04-28 10:29:39 -0600

    add method to login user

--- server/controllers/user/users.js
+++ server/controllers/user/users.js
@@ -1,5 +1,10 @@
 /***** User Controller *****/
 const User = require('../../models').User;
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const conf_var = require('../../config/config_var.js');
+
 module.exports = {
   create(req, res) {
     if(!req.is('application/json')){
@@ -10,7 +15,7 @@ module.exports = {
     }
     else{
       return User.create({
-        role_id: req.body.role_id,
+        role_id: 2,
         email: req.body.email,
         firstName: req.body.first_name,
         lastName: req.body.last_name,
@@ -18,6 +23,61 @@ module.exports = {
       })
       .then(user => res.status(201).send(user))
       .catch(error => res.status(400).send(error));
+    }
+  },
+  login(req, res){
+    if(!req.is('application/json')){
+      return res.status(403).send({
+        success: false,
+        message: 'Expected application/json'
+      });
+    }
+    else{
+      data = req.body || {};
+      return User.findOne({
+        where: {
+          email: data.email
+        },
+      })
+      .then(user =>{
+        if(!user) {
+          return res.status(404).send({
+            message: 'User email not found',
+          });
+        }
+        else{
+          if(!user.comparePassword(data.password)){
+            return res.status(404).send({
+              success: false,
+              message: "Password did not match"
+            });
+          }
+          else
+          {
+            if(user.role_id == 1 || user.role_id == 2){
+              res.status(200);
+              res.json({
+                token: jwt.sign(
+                  {
+                    user_id: user.id,
+                    role_id: user.role_id
+                  },
+                  conf_var.jwt_secret, { expiresIn: '4h'}
+                )
+              });
+            }
+            else{
+              return res.status(404).send({
+                success: false,
+                user: user,
+                message: "User does not have permission to login"
+              });
+            }
+          }
+
+
+        }
+      }).catch((error) => res.status(400).send(error));
     }
   },
 }
