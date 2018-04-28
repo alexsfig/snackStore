a5e5621bc18e4380a84e29731ed67510d36b3f77
alexsfig <alexandersosa2204@gmail.com>
2018-04-28 13:01:57 -0600

    fix relation ship errors

--- server/models/purchase_order.js
+++ server/models/purchase_order.js
@@ -1,11 +1,24 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var PurchaseOrder = sequelize.define('PurchaseOrder', {
-    user_id: DataTypes.INTEGER,
-    total_amount: DataTypes.DOUBLE,
-    status: DataTypes.BOOLEAN,
-    order_id: DataTypes.STRING
-  }, {
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    total_amount: {
+      type: DataTypes.DOUBLE,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.ENUM('pending', 'canceled', 'completed'),
+      allowNull: false
+    },
+    order_id: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
       freezeTableName: true,
       tableName: 'purchase_orders'
   });
@@ -18,7 +31,7 @@ module.exports = (sequelize, DataTypes) => {
     PurchaseOrder.hasMany(models.LineItem,
     {
       foreignKey: 'purchase_order_id',
-      as: 'purchase_order',
+      as: 'line_items',
     });
   };
   return PurchaseOrder;
