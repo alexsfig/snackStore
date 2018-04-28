262b35cdb444af0e4ce5176be69684274b571faf
alexsfig <alexandersosa2204@gmail.com>
2018-04-28 13:00:21 -0600

    add routes without login

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,24 +6,15 @@ module.exports = (app, jwt, express) =>
     const apiRoutes = express.Router();
 
 
-    apiRoutes.post('/products', productsController.create)
     apiRoutes.get('/products', productsController.list)
     apiRoutes.get('/products/:order', productsController.list)
     apiRoutes.get('/products/:order/:orderColumn', productsController.list)
     apiRoutes.get('/product/:id', productsController.find)
     apiRoutes.get('/productByName/:name', productsController.findByName)
     apiRoutes.put('/products/:id', productsController.update)
-    apiRoutes.delete('/products/:id', productsController.delete)
     apiRoutes.post('/products/:id/like', productsController.like)
-
-
-
-    /////////////
+    apiRoutes.post('/users/sign_in', usersController.login)
     apiRoutes.post('/users/sign_up', usersController.create)
-
-
-
-
 
     app.use('/api/v1', apiRoutes);
 
