99a43376be6649122ea6ea950908d1306fd2508e
alexsfig <alexandersosa2204@gmail.com>
2018-04-27 23:03:46 -0600

    Add estra field in product table

--- server/models/products.js
+++ server/models/products.js
@@ -1,14 +1,20 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var Products = sequelize.define('Products', {
+  var Product = sequelize.define('Product', {
     name: DataTypes.STRING,
     description: DataTypes.STRING,
     stock: DataTypes.INTEGER,
     price: DataTypes.DOUBLE,
-    likes: DataTypes.INTEGER
-  }, {});
-  Products.associate = function(models) {
+    likes: DataTypes.INTEGER,
+    status: DataTypes.BOOLEAN
+  },
+  {
+      freezeTableName: true,
+      tableName: 'Products'
+  }
+  );
+
+  Product.associate = function(models) {
     // associations can be defined here
   };
-  return Products;
+  return Product;
 };
