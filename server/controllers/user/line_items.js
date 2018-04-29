5848a564e8822e43784179064f59a22e7a457a42
alexsfig <alexandersosa2204@gmail.com>
2018-04-29 17:10:36 -0600

    Fix error in method detele on lineItems controller

--- server/controllers/user/line_items.js
+++ server/controllers/user/line_items.js
@@ -1,4 +1,5 @@
 const PurchaseOrder = require('../../models').PurchaseOrder;
+const Product = require('../../models').Product;
 const LineItem = require('../../models').LineItem;
 const purOrder = LineItem.belongsTo(PurchaseOrder, {as: 'purchase_orders', foreignKey: 'purchase_order_id'});
 const jwt = require('jsonwebtoken');
@@ -24,48 +25,83 @@ module.exports = {
 				});
 			}
 			else{
-        return PurchaseOrder.findOne({
-          attributes: ['order_id', 'id'],
-          where: {
-            user_id:  decoded.payload.user_id,
-            status:   "pending"
-          },
-        })
-        .then(purchaseOrder =>{
-          if(!purchaseOrder) {
-            return LineItem.create(
-              {
-                product_id: data.product_id,
-                price:      data.price,
-                total_line: data.quantity * data.price,
-                quantity:   data.quantity,
-                purchase_orders: {
-                  user_id:       decoded.payload.user_id,
-        					status:        'pending',
-                  order_id:      uuid.v4(),
-                }
-              },
-              {
-                include: [  purOrder  ]
-              }
-            )
-            .then(lineItems => res.status(201).send(lineItems))
-            .catch(error => res.status(400).send(error));
-          }
-          else{
-            return LineItem.create(
-              {
-                product_id:         data.product_id,
-                price:              data.price,
-                total_line:         data.quantity * data.price,
-                quantity:           data.quantity,
-                purchase_order_id: purchaseOrder.id
-              }
-            )
-            .then(lineItems => res.status(201).send(lineItems))
-            .catch(error => res.status(400).send(error));
-          }
-        }).catch((error) => res.status(400).send(error));
+				Product.findById(data.product_id)
+				.then(product =>{
+					if (product) {
+						if (product.stock >= data.quantity) {
+							return PurchaseOrder.findOne({
+								attributes: ['order_id', 'id'],
+								where: {
+									user_id:  decoded.payload.user_id,
+									status:   "pending"
+								},
+							})
+							.then(purchaseOrder =>{
+								if(!purchaseOrder) {
+									return LineItem.create(
+										{
+											product_id: data.product_id,
+											price:      product.price,
+											total_line: data.quantity * product.price,
+											quantity:   data.quantity,
+											purchase_orders: {
+												user_id:       	decoded.payload.user_id,
+												status:        	'pending',
+												order_id:      	uuid.v4(),
+												total_amount: 	0
+											}
+										},
+										{
+											include: [  purOrder  ]
+										}
+									)
+									.then(lineItems => {
+										product.update({
+											stock: (product.stock - data.quantity)
+										})
+										.then(purchase_order => res.status(200).send(lineItems))
+										.catch(error => res.status(400).send(error));
+										res.status(201).send(lineItems)
+									})
+									.catch(error => res.status(400).send(error));
+								}
+								else{
+									return LineItem.create(
+										{
+											product_id:         data.product_id,
+											price:              data.price,
+											total_line:         data.quantity * data.price,
+											quantity:           data.quantity,
+											purchase_order_id: purchaseOrder.id
+										}
+									)
+									.then(lineItems => {
+										product.update({
+											stock: product.stock - data.quantity
+										})
+										.then(purchase_order => res.status(200).send(lineItems))
+										.catch(error => res.status(400).send(error));
+									})
+									.catch(error => res.status(400).send(error));
+								}
+
+							}).catch((error) => res.status(400).send(error));
+						}
+						else{
+							res.status(404).send({
+								message: 'Product out of stock'
+							});
+						}
+					}
+					else{
+						res.status(404).send({
+							message: 'Product not found'
+						});
+					}
+
+				})
+				.catch((error) => res.status(400).send(error));
+
 			}
 
 		}
@@ -76,18 +112,34 @@ module.exports = {
 			LineItem.findById(req.params.id)
 			.then(line_item => {
 				if(!line_item){
-					res.status(404).send({
-						message: 'LineItem not found'
-					});
+	        res.status(404).send({
+	          message: 'LineItem not found'
+	        });
+	      }
+	      else{
+					return Product.findById(line_item.product_id)
+	        .then(product =>{
+	          product.update({
+	            stock: product.stock + line_item.quantity
+	          })
+	          .then(product => {
+	            line_item.destroy()
+	            .then(line_item => {
+	              res.status(200).send({
+	                message: 'LineItem has been deleted'
+	              });
+	            })
+	            .catch(error => res.status(400).send(error));
+	          })
+	          .catch(error => res.status(400).send(error));
+
+	        })
+	        .catch(error => res.status(400).send(error));
 				}
-				else{
-					line_item.destroy()
-					.then(() => res.status(200).send({
-						success: true,
-						message: 'LineItem deleted'
-					}))
-					.catch(error => res.status(400).send(error));
-				}
+
+
+
+
 			})
 			.catch(error => res.status(400).send(error));
 		}else{
