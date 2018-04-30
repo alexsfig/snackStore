678065908b2b34672521d2746bf273e611bb41ac
alexsfig <alexandersosa2204@gmail.com>
2018-04-30 01:50:17 -0600

    add x-access-token as valid header

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-access-token, Content-Length, X-Requested-With');
   res.setHeader('Access-Control-Allow-Credentials', true);
   if ('OPTIONS' == req.method) {
       res.sendStatus(200);
